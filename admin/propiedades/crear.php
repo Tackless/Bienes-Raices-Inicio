43772b21955a3fba30d143037793db2610fc8eab
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-03 16:48:15 -0600

    form crear

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -6,6 +6,49 @@
     <main class="contenedor seccion">
         <h1>Crear</h1>
         <a href="/admin/index.php" class="boton-verde">Volver</a>
+
+        <form class="formulario">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Título:</label>
+                <input type="text" id="titulo" placeholder="Titulo Propiedad">
+                
+                <label for="precio">Precio:</label>
+                <input type="text" id="precio" placeholder="Precio Propiedad">
+                
+                <label for="imagen">Imágen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción</label>
+                <textarea id="descripción" cols="30" rows="10"></textarea>
+
+            </fieldset>
+
+            <fieldset>
+                <legend>Información Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" placeholder="Ej: 3" min="1" max="9">
+                
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" placeholder="Ej: 3" min="1" max="9">
+                
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" placeholder="Ej: 3" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select name="" id="">
+                    <option value="1">Donovan</option>
+                    <option value="2">Karen</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton-verde">
+        </form>
     </main>
 
 <?php incluirTemplate('footer'); ?>
