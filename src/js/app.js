6b89252f35872309331d6a10252fd24fbc75dd42
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-03 13:06:15 -0600

    dark mode check

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,30 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function darkMode() {
+
+    // Dark Mode desde el sistema
+
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+
+
+    if (prefiereDarkMode) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+
+    prefiereDarkMode.addEventListener('change', function() {
+        if (prefiereDarkMode) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+
+    // Dark Mode desde un botón
+
     const botonDarkMode = document.querySelector('.dark-mode-boton');
+
     botonDarkMode.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
     })
