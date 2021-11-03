575e89f81444f38271eebcb0dea795ca377eb585
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-03 15:37:10 -0600

    php footer

--- anuncios.php
+++ anuncios.php
@@ -1,4 +1,4 @@
-<?php include 'includes/templates/header.php' ?>
+<?php include 'includes/templates/header.php'; ?>
 
     <main class="contenedor seccion">
         <section class="seccion contenedor">
@@ -206,20 +206,4 @@
         </section>
     </main>
 
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.php">Nosotros</a>
-                <a href="anuncios.php">Anuncios</a>
-                <a href="blog.php">Blog</a>
-                <a href="contacto.php">Contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Todos los derechos reservados Donovan Eliosa 2021 &copy;</p>
-    </footer>
-
-
-    <script src="/build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php include 'includes/templates/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 
     $inicio = true;
-    include 'includes/templates/header.php'
+    include 'includes/templates/header.php';
 ?>
 
     <main class="contenedor seccion">
@@ -202,20 +202,4 @@
         </section>
     </div>
 
-    <footer class="footer seccion">
-        <div class="contenedor contenedor-footer">
-            <nav class="navegacion">
-                <a href="nosotros.php">Nosotros</a>
-                <a href="anuncios.php">Anuncios</a>
-                <a href="blog.php">Blog</a>
-                <a href="contacto.php">Contacto</a>
-            </nav>
-        </div>
-
-        <p class="copyright">Todos los derechos reservados Donovan Eliosa 2021 &copy;</p>
-    </footer>
-
-
-    <script src="/build/js/bundle.min.js"></script>
-</body>
-</html>
+<?php include 'includes/templates/footer.php'; ?>
