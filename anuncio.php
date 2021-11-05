39fac4116832997899c51adb8f79295d12b1da78
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-04 20:07:42 -0600

    CRUD check

--- anuncio.php
+++ anuncio.php
@@ -1,18 +1,30 @@
 <?php 
 
-    // // Importar la base de datos
-    // require 'includes/config/database.php';
-    // $db = conectarBD();
+    $id = $_GET['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
 
-    // // Consultar la base de datos
-    // $query = "SELECT * FROM propiedades";
+    if (!$id) {
+        header('location: /');
+    }
 
-    // // Obtener los resultados
-    // $resultado = mysqli_query($db, $query);
-    // $propiedad = mysqli_fetch_assoc($resultado);
+    // Importar la base de datos
+    require 'includes/config/database.php';
+    $db = conectarBD();
 
-    // require 'includes/funciones.php';
-    // incluirTemplate('header');
+    // Consultar la base de datos
+    $query = "SELECT * FROM propiedades WHERE id = ${id}";
+
+    // Obtener los resultados
+    $resultado = mysqli_query($db, $query);
+
+    if (!$resultado->num_rows) {
+        header('location: /');
+    }
+
+    $propiedad = mysqli_fetch_assoc($resultado);
+
+    require 'includes/funciones.php';
+    incluirTemplate('header');
 
     // echo '<pre>';
     // var_dump($propiedad);
@@ -20,33 +32,34 @@
 ?>
 
     <main class="contenedor seccion contenido-centrado">
-        <!-- <h1><?php echo $resultado['titulo'];  ?></h1> -->
+        <h1><?php echo $propiedad['titulo'];  ?></h1>
 
-        <picture>
-            <source srcset="build/img/destacada.webp" type="image/webp">
-            <source srcset="build/img/destacada.jpg" type="image/jpeg">
-            <img loading="lazy" src="/build/img/destacada.jpg" alt="Imagen de la Propiedad">
-        </picture>
+        <img loading="lazy" src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Imagen de la Propiedad">
 
         <div class="resumen-propiedad">
-            <p class="precio">$3,000,000</p>
+            <p class="precio">$<?php echo number_format($propiedad['precio']); ?></p>
             <ul class="iconos-caracteristicas">
                 <li>
                     <img loading="lazy" class="iconos" src="/build/img/icono_wc.svg" alt="Icono wc">
-                    <p>3</p>
+                    <p><?php echo $propiedad['wc']; ?></p>
                 </li>
                 <li>
                     <img loading="lazy" class="iconos" src="/build/img/icono_estacionamiento.svg" alt="icono_estacionamiento">
-                    <p>3</p>
+                    <p><?php echo $propiedad['estacionamiento']; ?></p>
                 </li>
                 <li>
                     <img loading="lazy" class="iconos" src="/build/img/icono_dormitorio.svg" alt="icono_dormitorio">
-                    <p>3</p>
+                    <p><?php echo $propiedad['habitaciones']; ?></p>
                 </li> 
             </ul> 
 
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit adipisci delectus deleniti nemo tempora corrupti asperiores voluptate, dolore dolores alias voluptatibus, ab vero suscipit quas pariatur sit nobis! Assumenda, impedit. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit ab nesciunt consequatur, quas ad quam voluptatum sint nulla velit expedita omnis laboriosam voluptate similique animi alias illo placeat asperiores nam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore perferendis impedit sequi, consequuntur dolor eligendi reprehenderit nisi, inventore provident pariatur aliquam nemo quibusdam dolores commodi! Numquam ab dolores ipsam atque.</p>
+            <p><?php echo $propiedad['descripcion']; ?></p>
         </div>
     </main>
 
-<?php incluirTemplate('footer'); ?>
+<?php 
+
+    // Cerrar la conexión
+    mysqli_close($db);
+    incluirTemplate('footer'); 
+?>
