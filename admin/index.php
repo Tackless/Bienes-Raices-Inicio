62719f10ae29177439de772728ef5ea5f3f58d5f
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-07 11:51:28 -0600

    db to get properties as objects

--- admin/index.php
+++ admin/index.php
@@ -1,107 +1,95 @@
 <?php 
 
-    require '../includes/funciones.php';
-    $auth = estaAutenticado();
+require '../includes/app.php';
+estaAutenticado();
 
-    if (!$auth) {
-        header('Location: /');
-    }
+use App\Propiedad;
+// Implementar un método para obtener todas las propiedades
+$propiedades = Propiedad::all();
 
-    // Importar la conexión
-    require '../includes/config/database.php';
-    $db = conectarBD();
-
-    // Escribir el query
-    $query = "SELECT * FROM propiedades";
-
-    // Consultar la base de datos
-    $resultadoConsulta = mysqli_query($db, $query);
-
-    // Muestra mensaje condicional
-    $resultado = $_GET['resultado'] ?? null;
+// Muestra mensaje condicional
+$resultado = $_GET['resultado'] ?? null;
 
 
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $id = $_POST['id'];
-        $id = filter_var($id, FILTER_VALIDATE_INT);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST['id'];
+    $id = filter_var($id, FILTER_VALIDATE_INT);
 
-        if ($id) {
+    if ($id) {
 
-            // Eliminar el archivo
-            $query = "SELECT imagen FROM propiedades WHERE id = ${id}";
-            $resultado = mysqli_query($db, $query);
-            $propiedad = mysqli_fetch_assoc($resultado);
+        // Eliminar el archivo
+        $query = "SELECT imagen FROM propiedades WHERE id = ${id}";
+        $resultado = mysqli_query($db, $query);
+        $propiedad = mysqli_fetch_assoc($resultado);
 
-            unlink('../imagenes/' . $propiedad['imagen']);
+        unlink('../imagenes/' . $propiedad['imagen']);
 
-            // Elimina la propiedad
-            $query = "DELETE FROM propiedades WHERE id = ${id}";
-            $resultado = mysqli_query($db,$query);
+        // Elimina la propiedad
+        $query = "DELETE FROM propiedades WHERE id = ${id}";
+        $resultado = mysqli_query($db,$query);
 
-            if ($resultado) {
-                header('location: /admin?resultado=3');
-            }
+        if ($resultado) {
+            header('location: /admin?resultado=3');
         }
     }
+}
 
-    // Incluir el header
-    incluirTemplate('header');
+// Incluir el header
+incluirTemplate('header');
 ?>
 
-    <main class="contenedor seccion">
-        <h1>Administrador de Bienes Raices</h1>
+<main class="contenedor seccion">
+    <h1>Administrador de Bienes Raices</h1>
 
-        <?php if ($resultado === '1' ): ?>
-            <p class="alerta exito">Anuncio Creado Correctamente.</p>
-        <?php elseif ($resultado === '2'): ?>
-            <p class="alerta exito">Anuncio Actualizado Correctamente.</p>
-        <?php elseif ($resultado === '3'): ?>
-            <p class="alerta exito">Anuncio Eliminado Correctamente.</p>
-        <?php endif; ?>
+    <?php if ($resultado === '1' ): ?>
+        <p class="alerta exito">Anuncio Creado Correctamente.</p>
+    <?php elseif ($resultado === '2'): ?>
+        <p class="alerta exito">Anuncio Actualizado Correctamente.</p>
+    <?php elseif ($resultado === '3'): ?>
+        <p class="alerta exito">Anuncio Eliminado Correctamente.</p>
+    <?php endif; ?>
 
 
-        <a href="/admin/propiedades/crear.php" class="boton-verde">Nueva Propiedad</a>
+    <a href="/admin/propiedades/crear.php" class="boton-verde">Nueva Propiedad</a>
 
-        <table class="propiedades">
-            <thead>
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imágen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        
+        <tbody> <!-- Mostrar los resultados de la Base de Datos -->
+            <?php while ($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
+                
                 <tr>
-                    <th>ID</th>
-                    <th>Título</th>
-                    <th>Imágen</th>
-                    <th>Precio</th>
-                    <th>Acciones</th>
+                    <td> <?php echo $propiedad['id']; ?> </td>
+                    <td> <?php echo $propiedad['titulo']; ?> </td>
+                    <td> <img src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla" alt=""></td>
+                    <td> $ <?php echo number_format($propiedad['precio']); ?> </td>
+                    <td>
+                        <form method="POST" class="w-100" action="">
+
+                            <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
+
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a href="/admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
                 </tr>
-            </thead>
-            
-            <tbody> <!-- Mostrar los resultados de la Base de Datos -->
-                <?php while ($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
-                    
-                    <tr>
-                        <td> <?php echo $propiedad['id']; ?> </td>
-                        <td> <?php echo $propiedad['titulo']; ?> </td>
-                        <td> <img src="/imagenes/<?php echo $propiedad['imagen']; ?>" class="imagen-tabla" alt=""></td>
-                        <td> $ <?php echo number_format($propiedad['precio']); ?> </td>
-                        <td>
-                            <form method="POST" class="w-100" action="">
 
-                                <input type="hidden" name="id" value="<?php echo $propiedad['id']; ?>">
-
-                                <input type="submit" class="boton-rojo-block" value="Eliminar">
-                            </form>
-                            <a href="/admin/propiedades/actualizar.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Actualizar</a>
-                        </td>
-                    </tr>
-
-                <?php endwhile; ?>
-            </tbody>
-        </table>
-    </main>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
+</main>
 
 <?php 
 
-    // Cerrar la conexdión
-    mysqli_close($db);
+// Cerrar la conexdión
+mysqli_close($db);
 
-
-    incluirTemplate('footer'); 
-?>
+incluirTemplate('footer'); ?>
