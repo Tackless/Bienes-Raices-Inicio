9b97e2258809eafddbd023bc966c168973a46d99
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2021-11-07 19:34:41 -0600

    list vendedores in index

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@ use App\Vendedor;
 
 // Implementar un método para obtener todas las propiedades
 $propiedades = Propiedad::all();
-$vendedor = Vendedor::all();
+$vendedores = Vendedor::all();
 
 // Muestra mensaje condicional
 $resultado = $_GET['resultado'] ?? null;
@@ -43,6 +43,7 @@ incluirTemplate('header');
 
     <a href="/admin/propiedades/crear.php" class="boton-verde">Nueva Propiedad</a>
 
+    <h2>Propiedades</h2>
     <table class="propiedades">
         <thead>
             <tr>
@@ -63,9 +64,7 @@ incluirTemplate('header');
                     <td> $ <?php echo number_format($propiedad->precio); ?> </td>
                     <td>
                         <form method="POST" class="w-100" action="">
-
                             <input type="hidden" name="id" value="<?php echo $propiedad->id; ?>">
-
                             <input type="submit" class="boton-rojo-block" value="Eliminar">
                         </form>
                         <a href="/admin/propiedades/actualizar.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Actualizar</a>
@@ -74,11 +73,37 @@ incluirTemplate('header');
             <?php endforeach; ?>
         </tbody>
     </table>
+
+    <h2>Vendedores</h2>
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Telefono</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        
+        <tbody> <!-- Mostrar los resultados de la Base de Datos -->
+            <?php foreach( $vendedores as $vendedor ): ?>
+                <tr>
+                    <td> <?php echo $vendedor->id; ?> </td>
+                    <td> <?php echo $vendedor->nombre . " " . $vendedor->apellido; ?> </td>
+                    <td> <?php echo $vendedor->telefono; ?> </td>
+                    <td>
+                        <form method="POST" class="w-100" action="">
+                            <input type="hidden" name="id" value="<?php echo $vendedor->id; ?>">
+                            <input type="submit" class="boton-rojo-block" value="Eliminar">
+                        </form>
+                        <a href="/admin/vendedores/actualizar.php?id=<?php echo $vendedor->id; ?>" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 </main>
 
 <?php 
-
-// Cerrar la conexdión
-mysqli_close($db);
 
 incluirTemplate('footer'); ?>
